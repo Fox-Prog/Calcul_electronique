41a8e0728907f86c8fedd4bb5fc93617d22e10cd
Fox_Prog <FoxProg@protonmail.com>
2023-04-09 10:54:01 +0200

    MaJ calculs + Création ODS

--- Calibration_regulateur_tension.py
+++ Calibration_regulateur_tension.py
@@ -1,45 +1,47 @@
 
 # Caractéristiques du régulateur
 
-U_out = int(input("Tension de sortie... "))
+U_out = float(input("Tension de sortie... "))
+U_in_max = float(input("Tension d'entrée min... "))
 I_max = float(input("Intenstié max en sortie... "))
-I_max_reg = float(input("Intenstié max pour le régulateur... "))
 
-Hfe = int(input("Hfe transistor... "))
-Vbe = float(input("Vbe transistor... "))
+tr = input("Montage avec transistor ? ...Y/n... ")
+
+if tr == 'Y':
+    I_max_reg = float(input("Intenstié max pour le régulateur... "))
+    Hfe = int(input("Hfe transistor... "))
+    Vbe = float(input("Vbe transistor... "))
 
 # Calcul
     
 # U_in minimum ______________________________________________
-if U_out == 5:
-    U_in = 9
-elif U_out == 8:
-    U_in = 12
-else:
-    U_in = (1.4*U_out)
+U_in = (1.4*U_out)
 
 # C1 ______________________________________________
-C1 = 20000/(U_in/I_max)
+C1 = 20000/(U_in_max/I_max)
 
 # C4 ______________________________________________
 C4 = C1/10
 
-# R1 ______________________________________________
-Ib = I_max/Hfe
-I_R1 = I_max_reg - Ib
-R1 = Vbe/I_R1
-W_R1 = (I_R1*I_R1)*R1
+if tr == 'Y':
+    # R1 ______________________________________________
+    Ib = I_max/Hfe
+    I_R1 = I_max_reg - Ib
+    R1 = Vbe/I_R1
+    W_R1 = (I_R1*I_R1)*R1
 
-# R2 ______________________________________________
-R2 = Vbe/I_max
+    # R2 ______________________________________________
+    R2 = Vbe/I_max
 
 print('\n')
 
-print(f"Tension d'entré minimum: {round(U_in, 3)}")
-print(f"C1: {round(C1, 3)} uF")
-print(f"C4: {round(C4, 3)} uF")
-print(f"R1: {round(R1, 3)} ohms, {round(W_R1, 3)} watt")
-print(f"R2: {round(R2, 3)} ohms")
+print(f"Tension d'entrée minimum: {round(U_in, 3)} ( A comfirmer...)")
+print(f"C1: {round(C1)} uF  (min)")
+print(f"C4: {round(C4)} uF  (min)")
+
+if tr == 'Y':
+    print(f"R1: {round(R1, 3)} ohms, {round(W_R1, 3)} watt")
+    print(f"R2: {round(R2, 3)} ohms, 3 watt")
 
 print('\n')
 
--- Dissipation_thermique.py
+++ Dissipation_thermique.py
@@ -1,3 +1,5 @@
+import pyexcel_ods3
+import os
 
 def Types():
     typ_calc = input('''
@@ -20,18 +22,17 @@ def Types():
 
 
 
-
-
-
-
-
 def Calcul_TR():
     # Caractéristique du transistor
     print('\n')
     print("_____ Caractéristiques du transistor _____")
     print('\n')
-    Vce = float(input("Vce(sat)... "))
-    Ic = float(input("I collecteur... "))
+    U_in = float(input("U in MAX... "))
+    U_out = float(input("U out... "))
+    Ic = float(input("I out... "))
+    Vce_sat = float(input("Vce(sat)... "))
+
+    Vce = (U_in - U_out) - Vce_sat
     Pdiss = (Vce*Ic)
 
     print("Datasheet : Operating junction temperature")
@@ -65,12 +66,18 @@ def Calcul_TR():
     # SAVE
     Suite = input('''
     1 >>> Enregistrer les résultats
-    2 >>> Recalculer
+    2 >>> Enregister les résultats + Graphe Ta et C/W_Diss pour I
+    3 >>> Recalculer
     >>> ... ''')
 
-    if Suite == '1':
-        name = str(input("Nom du document... ")+'.txt')
-        file = open(name, 'a', encoding='utf-8')
+    if Suite == '1' or Suite =='2':
+        os.chdir('C:/Users/Utilisateur/Desktop')
+
+        name_brut = input("Nom du document... ")
+        nametxt = str(name_brut+'.txt')
+        nameods = str(name_brut+'.ods')
+
+        file = open(nametxt, 'a', encoding='utf-8')
         file.write('\n')
         file.write('_____ Caracteristiques du transistor _____'+'\n')
 
@@ -88,9 +95,34 @@ def Calcul_TR():
         file.write('Temperature ambiente max sans radiateur: '+str(round(Ta_max, 2))+' \u00b0C'+'\n')
         file.write('Resistance thermique min du dissipateur: '+str(round(Rth_diss, 2))+' \u00b0C/W'+'\n')
 
-
         file.close()
-    elif Suite == '2':
+
+
+    if Suite == '2':
+        # Création du tableur
+        title_spe = ["Vce", "Tj", "Rth_jc", "Rth_ja"]
+        value_spe = [str(Vce), str(Tj), str(Rth_jc), str(Rth_ja)]
+        titles = ["Intensité(A)", "Ta_max", "Rth_Diss"]
+        data = []
+        data.append(title_spe)
+        data.append(value_spe)
+        data.append("")
+        data.append(titles)
+        ic = 0.2
+        while ic < 15.2:
+            Pdiss = (Vce*ic)
+            Ta_max = Tj - (Pdiss*Rth_ja)
+            Pdiss_mrg = (Pdiss + (Pdiss/4))
+            Rth_diss = ((Tj - Ta)/Pdiss_mrg) - (Rth_jc + 0.5)
+            
+            row = [ic, Ta_max, Rth_diss]
+            data.append(row)
+            ic += 0.2
+
+        pyexcel_ods3.save_data(str('Graph_tr_' + nameods), {"Ta_max__Rth_Diss pour Ic":data})
+
+
+    elif Suite == '3':
         Types()
     else:
         exit() 
@@ -148,13 +180,22 @@ def Calcul_reg():
 
     # SAVE
     Suite = input('''
-    1 >>> Enregistrer les résultats
-    2 >>> Recalculer
+    1 >>> Enregistrer les résultats simple
+    2 >>> Enregister les résusltats + graph Imax pour T
+    3 >>> Enregister les résusltats + graph Rth_diss pour U_in
+    4 >>> Enregistrer tout
+    5 >>> Recalculer
     >>> ... ''')
 
-    if Suite == '1':
-        name = str(input("Nom du document... ")+'.txt')
-        file = open(name, 'a', encoding='utf-8')
+    if Suite == '1' or Suite == '2' or Suite == '3' or Suite == '4':
+        os.chdir('C:/Users/Utilisateur/Desktop')
+
+        # Création des fichiers
+        name_brut = input("Nom du document... ")
+        nametxt = str(name_brut+'.txt')
+        nameods = str(name_brut+'.ods')
+
+        file = open(nametxt, 'a', encoding='utf-8')
         file.write('\n')
         file.write('_____ Caracteristiques du regulateur _____'+'\n')
 
@@ -172,9 +213,50 @@ def Calcul_reg():
         file.write('Intensite max de sortie sans dissipateur: '+str(round(Is, 2))+' A'+'\n')
         file.write('Resistance thermique min du dissipateur: '+str(round(Rth_diss, 2))+' \u00b0C/W'+'\n')
 
-
         file.close()
-    elif Suite == '2':
+
+    if Suite == '2' or Suite == '4':
+        # Création du tableur
+        title_spe = ["U_in", "U_out", "Tj", "Rth_ja", "Rth_jc"]
+        value_spe = [str(U_in), str(U_out), str(Tj), str(Rth_ja), str(Rth_jc)]
+        titles = ["Ta(°C)", "Imax(A)", "Rth_diss(°C/W)"]
+        data = []
+        data.append(title_spe)
+        data.append(value_spe)
+        data.append("")
+        data.append(titles)
+        for t in range(-20, 61):
+            Is = ((Tj - t)/Rth_ja) / (U_in - U_out)
+            Is_calc = round(Is, 2)
+            Rth_diss = ((Tj-t)-(((U_in-U_out)*I_out)*Rth_jc)) / ((U_in-U_out)*I_out)
+            Rth_diss_calc = round(Rth_diss, 2)
+            row = [t, Is_calc, Rth_diss_calc]
+            data.append(row)
+
+        pyexcel_ods3.save_data(str('Imax for Ta_' + nameods), {"Imax pour T":data})
+
+
+    if Suite == '3' or Suite == '4':
+        # Création du tableur
+        title_spe = ["U_out", "I_out", "Tj", "Ta", "Rth_ja", "Rth_jc"]
+        value_spe = [str(U_out), str(I_out), str(Tj), str(Ta), str(Rth_ja), str(Rth_jc)]
+        titles = ["U_in", "Rth_diss(°C/W)"]
+        data = []
+        data.append(title_spe)
+        data.append(value_spe)
+        data.append("")
+        data.append(titles)
+        for u in range(9, 36):
+            Rth_diss = ((Tj-Ta)-(((u-U_out)*I_out)*Rth_jc)) / ((u-U_out)*I_out)
+            Rth_diss_calc = round(Rth_diss, 2)
+            row = [u, Rth_diss_calc]
+            data.append(row)
+
+        pyexcel_ods3.save_data(str('Rth_diss for U_in_'+ nameods), {"Rth_diss pour U_in":data})
+
+
+
+    elif Suite == '5':
         Types()
     else:
         exit()        
